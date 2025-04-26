079a319531d71d41966fd62349447c281ba0d711
h-akira <79441679+h-akira@users.noreply.github.com>
2025-04-26 15:05:19 +0900

    add home

--- Lambda/project/urls.py
+++ Lambda/project/urls.py
@@ -1,11 +1,11 @@
 from hads.urls import Path, Router
-from .views import index, sample, logout
+# from .views import index, sample, logout
 
 urlpatterns = [
   # Path("AAA/MMM/NNN", "AAAFunction", name="AAA"),
-  Path("", index, name="home"),
-  Path("home", index, name="home2"),
-  Path("sample/{parameter}", sample, name="sample"),
-  Path("logout", logout, name="logout"),
-  Router("example", "example.urls", name="example")
+  # Path("", index, name="home"),
+  # Path("home", index, name="home2"),
+  # Path("sample/{parameter}", sample, name="sample"),
+  # Path("logout", logout, name="logout"),
+  Router("", "kifu.urls", name="example")
 ]
