8e0219300626982b7ce1c03f3b4dfa3e371c2477
h-akira <79441679+h-akira@users.noreply.github.com>
2025-04-30 23:28:02 +0900

    fix

--- Lambda/kifu/urls.py
+++ Lambda/kifu/urls.py
@@ -3,5 +3,5 @@ from .views import index, explorer
 
 urlpatterns = [
   Path("{username}", index, name="index"),
-  Path("{username}/explorer", exploler, name="exploler"),
+  Path("{username}/explorer", explorer, name="explorer")
 ]
