aa1481787e3daaa0270fcc21a2102b815028ac20
h-akira <79441679+h-akira@users.noreply.github.com>
2025-05-01 21:32:30 +0900

    update

--- Lambda/kifu/views.py
+++ Lambda/kifu/views.py
@@ -1,11 +1,15 @@
 from hads.shourtcuts import render
+from hads.shourtcuts import login_required, redirect
 
+
+@login_required
 def index(master, username):
   context = {
     'username': username
   }
   return render(master, 'kifu/index.html', context)
 
+@login_required
 def explorer(master, username):
   context = {
     'username': username
