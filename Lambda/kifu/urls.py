357d8f834b78bb55f74a20e6c355874e8be938f2
h-akira <79441679+h-akira@users.noreply.github.com>
2025-05-05 21:03:24 +0900

    update

--- Lambda/kifu/urls.py
+++ Lambda/kifu/urls.py
@@ -1,11 +1,11 @@
 from hads.urls import Path
-from .views import index, explorer, create, edit
+from .views import index, explorer, create, edit, detail
 
 urlpatterns = [
   Path("index/{username}", index, name="index"),
   Path("explorer/{username}", explorer, name="explorer"),
   Path("create/{username}", create, name="create"),
-  # Path("{username}/detail/{kid}", create, name="create"),
+  Path("detail/{username}/{kid}", create, name="create"),
   Path("edit/{username}/{kid}", edit, name="edit")
   # Path("{username}/delete/{kid}", delete, name="create"),
 ]
