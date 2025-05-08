0b8433f714c0dbb37e2f346e4a493a6b2c086176
h-akira <79441679+h-akira@users.noreply.github.com>
2025-05-08 23:36:59 +0900

    update

--- Lambda/analysis/views.py
+++ Lambda/analysis/views.py
@@ -10,10 +10,34 @@ from zoneinfo import ZoneInfo
 MAIN_TABLE_NAME = "table-sgp-main"
 AID_LENGTH = 8
 
+@login_required
 def submit(master):
-  response = {"hoge": "fuga"}
+  table = boto3.resource('dynamodb').Table(MAIN_TABLE_NAME)
+  response = table.query(
+    KeyConditionExpression=Key('pk').eq('analysis'),
+    IndexName="CreatdIndex",
+    ScanIndexForward=False
+  )
+  master.logger.info(f"response: {response}")
+  now = datetime.datetime.now(ZoneInfo(master.settings.TIMEZONE))
+  now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+  expired = now + datetime.timedelta(hours=1)
+  expired_int = int(expired.timestamp())
+  Item = {
+    "pk": "analysis",
+    "sk": f"aid#{gen_code(AID_LENGTH)}",
+    "cgsi_pk": f"analysis#uname#{master.request.username}",
+    "created": now_str,
+    "status": "waiting",
+    "response": None,
+    "expired": expired_int
+  }
+  response = table.put_item(
+    Item=Item
+  )
   return json_response(response)
 
+@login_required
 def inquire(master, aid):
   response = {"hoge": "fuga"}
   return json_response(response)
