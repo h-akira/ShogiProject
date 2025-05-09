925e714e794bfbbb7dba3d812907e72e978f0d35
h-akira <79441679+h-akira@users.noreply.github.com>
2025-05-09 21:29:32 +0900

    update

--- Lambda/analysis/views.py
+++ Lambda/analysis/views.py
@@ -12,20 +12,49 @@ AID_LENGTH = 8
 
 @login_required
 def submit(master):
+  position = "lnsgkgsnl/1r5b1/p1pppp1p1/6p1p/9/2P6/PP1PPPPPP/1B5R1/LNSGKGSNL b - 1"
+  sqs = boto3.client('sqs')
+  ssm = boto3.client('ssm')
+  QueueUrl=ssm.get_parameter(Name="/ShogiProject/SQS/Analysis/URL")["parameter"]["Value"]
+  # キュー内のメッセージの数が多ければ拒否
+  response = sqs.get_queue_attributes(
+    QueueUrl=QueueUrl,
+    AttributeNames=['ApproximateNumberOfMessages']
+  )
+  if int(response['Attributes']['ApproximateNumberOfMessages']) > 5:
+    return json_response(
+      master, 
+      {
+        "submit": "reject",
+        "aid": None
+      }
+    )
+  # 直近の実行が一定以上なら拒否
+  now = datetime.datetime.now(ZoneInfo(master.settings.TIMEZONE))
+  now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+  ago_str = (now - datetime.timedelta(hours=1)).strftime("%Y-%m-%d %H:%M:%S")
   table = boto3.resource('dynamodb').Table(MAIN_TABLE_NAME)
   response = table.query(
     KeyConditionExpression=Key('pk').eq('analysis'),
     IndexName="CreatedIndex",
-    ScanIndexForward=False
+    ScanIndexForward=False,
+    Limit=30,
   )
-  master.logger.info(f"response: {response}")
-  now = datetime.datetime.now(ZoneInfo(master.settings.TIMEZONE))
-  now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+  if len(response["Items"]) == 30 and response["Items"][-1]["created"] > ago_str:
+    return json_response(
+      master, 
+      {
+        "submit": "reject",
+        "aid": None
+      }
+    )
+  # DynamoDBに登録
   expired = now + datetime.timedelta(hours=1)
   expired_int = int(expired.timestamp())
+  aid = gen_code(AID_LENGTH)
   Item = {
     "pk": "analysis",
-    "sk": f"aid#{gen_code(AID_LENGTH)}",
+    "sk": f"aid#{aid}",
     "cgsi_pk": f"analysis#uname#{master.request.username}",
     "created": now_str,
     "status": "waiting",
@@ -35,7 +64,26 @@ def submit(master):
   response = table.put_item(
     Item=Item
   )
-  return json_response(master, response)
+  # SQSに送信
+  data = {
+    "username": master.request.username,
+    "aid": aid,
+    "position": position
+  }
+  message = json.dumps(data)
+  response = sqs.send_message(
+    QueueUrl=QueueUrl,
+    MessageBody=message,
+    MessageGroupId=data["username"],
+    MessageDeduplicationId=data["aid"]
+  )
+  return json_response(
+    master, 
+    {
+      "submit": "accept",
+      "aid": aid
+    }
+  )
 
 @login_required
 def inquire(master, aid):
