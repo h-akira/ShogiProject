c3a11b8c728b3904dad23aaf2be07444c15c51a3
h-akira <79441679+h-akira@users.noreply.github.com>
2025-08-21 22:33:48 +0900

    remove file

--- Lambda/example/urls.py
+++
@@ -1,6 +0,0 @@
-from hads.urls import Path
-from .views import index
-
-urlpatterns = [
-  Path("home", index, name="home")
-]
--- Lambda/example/views.py
+++
@@ -1,5 +0,0 @@
-from hads.shortcuts import render
-
-def index(master):
-  context = {}
-  return render(master, 'example/index.html', context)
